4afb57701cc05c3ad36927cb709606a0a09d0057
bion-slmn <bionsol25@gmail.com>
2023-09-22 02:43:55 -0700

    create

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # this creates a file in /tmp
 
 file {'/tmp/school':
-  ensure  => present
-  mode    => '0744'
-  owner   => www-data
-  group   => www-data
-  content => 'I love Puppet'
+  ensure  => 'present',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
 }
