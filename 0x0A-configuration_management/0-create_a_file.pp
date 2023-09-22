71c2ab58e8265f17b120c42be6ea6852993a2598
bion-slmn <bionsol25@gmail.com>
2023-09-22 12:48:16 +0300

    my first puppet file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,7 +3,7 @@
 file {'/tmp/school':
   ensure  => present
   mode    => '0744'
-  owner   => www-data
-  group   => www-data
+  owner   => 'www-data'
+  group   => 'www-data'
   content => 'I love Puppet'
 }
