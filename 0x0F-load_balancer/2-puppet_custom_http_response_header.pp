0e948e310158b84b2e0f17493a7941143005b65f
bion-slmn <bionsol25@gmail.com>
2023-10-02 12:39:07 -0700

    using puppet to configure

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,40 @@
+# installing ngnix with the latest
+
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file { '/etc/nginx/html':
+  ensure => 'directory',
+}
+
+file { '/etc/nginx/html/index.html':
+  ensure  => 'present',
+  content => 'Hello World!',
+}
+
+file { '/etc/nginx/sites-available/default':
+        ensure => 'present',
+        content => 'server {
+     listen      80 default_server;
+     listen      [::]:80 default_server;
+     root        /etc/nginx/html;
+     index       index.html index.htm;
+     add_header  X-Served-By $HOSTNAME
+
+     location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+
+    error_page 404 /custom_404.html;
+    location = /custom_404.html {
+        root /etc/nginx/html;
+        internal;
+        }
+}',
+}
+
+service { 'nginx':
+  ensure    => 'running',
+  enable    => true,
+}
