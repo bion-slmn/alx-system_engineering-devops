9b315205a1cd97b4372c7dba510d9165e851d535
bion-slmn <bionsol25@gmail.com>
2023-10-23 08:26:59 -0700

    correcting the payload

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,11 +9,13 @@ import sys
 if __name__ == '__main__':
     employee_ID = sys.argv[1]
     url = 'https://jsonplaceholder.typicode.com'
-    payload = {"userId": employee_ID}
-    user_response = requests.get(f'{url}/users/{employee_ID}').json()
-    todo_response = requests.get(f'{url}/todos/', params=payload).json()
+    payload = {"id": employee_ID}
+    pay = {"userId": employee_ID}
+    user_response = requests.get(f'{url}/users/', params=payload).json()
 
-    employee_name = user_response.get('name')
+    todo_response = requests.get(f'{url}/todos/', params=pay).json()
+
+    employee_name = user_response[0].get("username")
     file_name = f'{employee_ID}.csv'
 
     with open(file_name, 'w', encoding='utf-8') as f:
