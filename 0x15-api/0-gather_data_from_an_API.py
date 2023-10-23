737045d7811200938551f93b510b5875e48eae59
bion-slmn <bionsol25@gmail.com>
2023-10-23 06:45:53 -0700

    reading from an api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''THis script uses a RESTAPI to collect information
+of an employee'''
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    employee_ID = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com'
+    payload = {"userId": employee_ID}
+    user_response = requests.get(f'{url}/users/{employee_ID}').json()
+    todo_response = requests.get(f'{url}/todos/', params=payload).json()
+
+    employee_name = user_response.get('name')
+    total_task = len(todo_response)
+
+    task_completd = filter(lambda x: x.get("completed") is True,
+                           todo_response)
+    task_completd_list = list(task_completd)
+
+    print(f'Employee {employee_name} is done with tasks\
+          ({len(task_completd_list)}/{total_task}): ')
+
+    for task in task_completd_list:
+        print(f'     {task.get("title")}')
---
+++ 0x15-api/README.md
@@ -0,0 +1,2 @@
+This is a project about API and restful Api.
+
