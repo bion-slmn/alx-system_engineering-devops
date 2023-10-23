9f6bcbffdf7a68ea2664fdf90018aeaf87144c3d
bion-slmn <bionsol25@gmail.com>
2023-10-23 11:17:46 -0700

    removed f string

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -16,17 +16,12 @@ if __name__ == '__main__':
     todo_response = requests.get(f'{url}/todos/', params=pay).json()
 
     employee_name = user_response[0].get("username")
-    file_name = f'{employee_ID}.json'
+    file_name = '{}.json'.format(employee_ID)
 
-    todo_dict = {
-              employee_ID: [
-                    {
-                            "task": task.get("title"),
-                            "completed": task.get("completed"),
-                            "username": employee_name
-                    }
-                    for task in todo_response
-                           ]
-              }
+    todo_dict = {employee_ID: [{
+                                "task": task.get("title"),
+                                "completed": task.get("completed"),
+                                "username": employee_name
+                    }for task in todo_response]}
     with open(file_name, 'w') as json_file:
         json.dump(todo_dict, json_file)
