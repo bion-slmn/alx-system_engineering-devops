56994300b1da962e42e8af117b8b7efd18a1e4e2
bion-slmn <bionsol25@gmail.com>
2023-10-23 09:22:28 -0700

    exporting json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''THis script uses a RESTAPI to collect information
+of an employee and writes it to a .csv file'''
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    employee_ID = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com'
+    payload = {"id": employee_ID}
+    pay = {"userId": employee_ID}
+    user_response = requests.get(f'{url}/users/', params=payload).json()
+
+    todo_response = requests.get(f'{url}/todos/', params=pay).json()
+
+    employee_name = user_response[0].get("username")
+    file_name = f'{employee_ID}.json'
+
+    todo_dict = {
+              employee_ID: [
+                    {
+                            "task": task.get("title"),
+                            "completed": task.get("completed"),
+                            "username": employee_name
+                    }
+                    for task in todo_response
+                           ]
+              }
+    with open(file_name, 'w', encoding='utf-8') as json_file:
+        json.dump(todo_dict, json_file)
