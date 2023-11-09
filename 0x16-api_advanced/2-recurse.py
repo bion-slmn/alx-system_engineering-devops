9e01d0871d77575f923e4cf9f3eae45965a435b3
bion-slmn <bionsol25@gmail.com>
2023-11-09 09:06:51 +0300

    recursision

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,7 +3,7 @@
 import requests
 
 
-def recurse(subreddit, after=None, hotlist=[]):
+def recurse(subreddit, after=None, hot_list=[]):
     ''' returns all hot posts listed for a given subreddit.'''
 
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
@@ -19,13 +19,13 @@ def recurse(subreddit, after=None, hotlist=[]):
                             params=params, allow_redirects=False)
     if response.status_code == 404:
         return None
-    results = response.json()
+    results = response.json().get('data')
 
-    for item in results.get('data').get("children"):
-        hotlist.append(item.get('data').get('title'))
+    for item in results.get("children"):
+        hot_list.append(item.get('data').get('title'))
 
-    after = results.get('data').get('after')
+    after = results.get('after')
 
     if after:
-        recurse(subreddit, after, hotlist)
-    return hotlist
+        recurse(subreddit, after, hot_list)
+    return hot_list
